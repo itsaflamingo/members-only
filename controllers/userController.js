ce327ca12e3f5393dbefbb2e4d017491ed8dc23c
Liz <elizabeth.pinero.r@gmail.com>
2023-02-21 16:45:39 -0500

    Signup form now displays errors if failed and navigates to chat page if success

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,72 @@
 const User = require('../models/user');
 const async = require('async');
-const { body, validationResult } = require('express-validator');
-
-exports.user_list = (req, res, next) => {
-    User.find({}, 'first_name last_name')
-        .sort({ name: 1 })
-        exec(function(err, list_user) {
-            if(err) {
-                return next(err);
-            }
-            res.render('user_list', { title: 'List of Users', user_list: list_user})
-        })
-}
+const { body, validationResult, custom, check } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 exports.user_create_get = (req, res, next) => {
     res.render('sign-up', { title: "Sign Up" });
 }
 
 // Sign up
-exports.user_create_post = (req, res, next) => {
-    const user = new User({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        username: req.body.username,
-        password: req.body.password
-    }).save(err => {
-        console.log(res);
-        if(err) {
-            return next(err);
+exports.user_create_post = [
+    body('first_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('First name must be specified')
+        .isAlphanumeric()
+        .withMessage('First name has non-alphanumeric characters'),
+    body('last_name')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Last name must be specified')
+        .isAlphanumeric()
+        .withMessage('Last name has non-alphanumeric characters'),
+    body('username')
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage('Username must be specified')
+        .isAlphanumeric()
+        .withMessage('Username has non-alphanumeric characters'),
+    body('password')
+        .trim()
+        .isLength({ min: 2 })
+        .escape()
+        .withMessage('Password must be specified'),
+    check(
+        'confirm_password',
+        'confirm password must have the same value as password field'
+        )
+        .exists()
+        .custom((value, { req }) => value === req.body.password),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.render('sign-up', {
+                title: 'Sign Up',
+                errors: errors.array()
+            });
         }
-        res.redirect('/');
-    })
-}
+        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+            if(err) {
+                return console.log(err);
+            }
+            // Else add new user to db
+            const user = new User({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                username: req.body.username,
+                password: hashedPassword,
+                confirm_password: req.body.confirm_password,
+                membership_status: 'Member'
+            }).save(err => {
+                if(err) {
+                    return next(err);
+                }
+                res.render('chat', { user: req.body });
+            })
+        })    
+    }
+]
--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const userSchema = new Schema({
     last_name:         { type: String, required: true },
     username:          { type: String, required: true },
     password:          { type: String, required: true },
+    confirm_password:  { type: String, required: true },
     membership_status: { type: String, enum: ['Member', 'Not member'] }
 })
 
